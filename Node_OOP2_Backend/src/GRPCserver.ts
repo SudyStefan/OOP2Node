6b3ec6c9b9ce78f71ca9d5b92bb7a120cf66903c
STEFAN SUDY <stefan.sudy@student.tugraz.at>
2024-11-23 12:28:49 +0100

    establish grpc connection

--- Node_OOP2_Backend/src/GRPCserver.ts
+++ Node_OOP2_Backend/src/GRPCserver.ts
@@ -2,10 +2,10 @@ import * as grpc from "@grpc/grpc-js"
 import * as protoLoader from "@grpc/proto-loader"
 import { Repository } from "./Repository";
 import fs from 'fs'
-import { Amenity } from "./Amenity";
-import { Road } from "./Roads";
-import { CRS } from "./CRS";
-import { Entity } from "./Entity";
+import { Amenity } from "../../shared/models/Amenity";
+import { Road } from "../../shared/models/Roads";
+import { CRS } from "../../shared/models/CRS";
+import { EntityProto, CRSProto, AmenityGeometryProto, AmenityProto, CoordinateList, RoadGeometryProto, RoadProto } from "../../shared/models/ProtoModels";
 
 function readJSON<T>(filePath: string, ctor: new (...args: any[]) => T): Map<number, T> {
   try {
@@ -25,7 +25,7 @@ function readJSON<T>(filePath: string, ctor: new (...args: any[]) => T): Map<num
   }
 }
 
-const PROTO_PATH = __dirname + "./../proto/mapservice.proto";
+const PROTO_PATH = "./../shared/proto/mapservice.proto";
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
   keepCase: true,
   longs: String,
@@ -34,8 +34,37 @@ const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
   oneofs: true
 });
 
-const amenities: Map<number, Amenity> = readJSON<Amenity>("./data/amenities.json", Amenity);
-const roads: Map<number, Road> = readJSON<Road>("./data/roads.json", Road);
+const amenityData = JSON.parse(fs.readFileSync("./data/amenities.json", 'utf-8'));
+const amenities = new Map<number, Amenity>(
+  amenityData.map((item: any) => [
+    item.id, 
+    new Amenity(
+      item.name, 
+      item.id, 
+      item.tags, 
+      item.type, 
+      item.geom.type, 
+      item.geom.coordinates, 
+      new CRS(item.geom.crs.type, item.geom.crs.properties)
+    )
+  ])
+);
+
+const roadData = JSON.parse(fs.readFileSync("./data/roads.json", 'utf-8'));
+const roads = new Map<number, Road>(
+  roadData.map((item: any) => [
+    item.id, 
+    new Road(
+      item.name, 
+      item.id, 
+      item.tags, 
+      item.type, 
+      item.geom.type, 
+      item.geom.coordinates, 
+      new CRS(item.geom.crs.type, item.geom.crs.properties)
+    )
+  ])
+);
 
 Repository.initRepository(amenities, roads);
 
@@ -43,15 +72,73 @@ const mapServiceProt: any = grpc.loadPackageDefinition(packageDefinition).mapser
 
 const amenityById = (call: grpc.ServerUnaryCall<any, any>, callback: grpc.sendUnaryData<any>) => {
   const { id } = call.request;
-  const amenity = Repository.getAmenities().get(id);
-  callback(null, { amenity });
+  const amenity = Repository.getAmenities().get(parseInt(id));
+  if (amenity !== undefined) {
+    callback(null, amenityToProto(amenity));
+  } else {
+    callback({code: grpc.status.NOT_FOUND, message: `No amenity found with id: ${id}`});
+  }
+};
+
+const roadById = (call: grpc.ServerUnaryCall<any, any>, callback: grpc.sendUnaryData<any>) => {
+  const { id } = call.request;
+  const road = Repository.getRoads().get(id);
+  callback(null, { road });
 };
 
 const server = new grpc.Server();
-server.addService(mapServiceProt.MapService.service, { amenityById });
+server.addService(mapServiceProt.MapService.service, { amenityById, roadById });
 const adress = "localhost:8020";
 
 server.bindAsync(adress, grpc.ServerCredentials.createInsecure(), () => {
   console.log("Server started at localhost:8020");
 });
 
+function amenityToProto(amenity: Amenity): AmenityProto {
+
+  const entityProto: EntityProto = {
+    name: amenity.name,
+    id: amenity.id,
+    tags: amenity.tags,
+    type: amenity.type
+  };
+
+  const geomProto: AmenityGeometryProto = {
+    type: amenity.geom.type,
+    coordinates: amenity.geom.coordinates,
+    crs: amenity.geom.crs
+  };
+
+  const proto: AmenityProto = {
+    entity: entityProto,
+    geom: geomProto
+  }; 
+
+  return proto;
+}
+
+function roadToProto(road: Road): RoadProto {
+  const entityProto: EntityProto = {
+    name: road.name,
+    id: road.id,
+    tags: road.tags,
+    type: road.type
+  };
+  
+  const coords: CoordinateList[] = road.geom.coordinates.map(inner => (
+    { coordinates: inner }
+  ));
+
+  const geomProto: RoadGeometryProto = {
+    type: road.geom.type,
+    coordinates: coords,
+    crs: road.geom.crs
+  };
+  
+  const proto: RoadProto = {
+    entity: entityProto,
+    geom: geomProto
+  }; 
+
+  return proto;
+}
